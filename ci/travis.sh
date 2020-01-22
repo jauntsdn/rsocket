6bd52d31bb30d557f3d40afd93ebfc7af73c3a08
Maksym Ostroverkhov <m.ostroverkhov@gmail.com>
2020-01-22 15:26:09 +0200

    update travisfile

--- ci/travis.sh
+++ ci/travis.sh
@@ -1,32 +1,13 @@
 #!/usr/bin/env bash
 
 if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
-
     echo -e "Building PR #$TRAVIS_PULL_REQUEST [$TRAVIS_PULL_REQUEST_SLUG/$TRAVIS_PULL_REQUEST_BRANCH => $TRAVIS_REPO_SLUG/$TRAVIS_BRANCH]"
-    ./gradlew build
-
-elif [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_TAG" == "" ] && [ "$bintrayUser" != "" ] ; then
-
-    echo -e "Building Snapshot $TRAVIS_REPO_SLUG/$TRAVIS_BRANCH"
-    ./gradlew \
-        -PbintrayUser="${bintrayUser}" -PbintrayKey="${bintrayKey}" \
-        -PsonatypeUsername="${sonatypeUsername}" -PsonatypePassword="${sonatypePassword}" \
-        -PversionSuffix="-SNAPSHOT" \
-        build artifactoryPublish --stacktrace
-
-elif [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_TAG" != "" ] && [ "$bintrayUser" != "" ] ; then
-
+elif [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_TAG" == "" ] ; then
+    echo -e "Building branch $TRAVIS_REPO_SLUG/$TRAVIS_BRANCH"
+elif [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_TAG" != "" ] ; then
     echo -e "Building Tag $TRAVIS_REPO_SLUG/$TRAVIS_TAG"
-    ./gradlew \
-        -Pversion="$TRAVIS_TAG" \
-        -PbintrayUser="${bintrayUser}" -PbintrayKey="${bintrayKey}" \
-        -PsonatypeUsername="${sonatypeUsername}" -PsonatypePassword="${sonatypePassword}" \
-        build bintrayUpload --stacktrace
-
 else
-
     echo -e "Building $TRAVIS_REPO_SLUG/$TRAVIS_BRANCH"
-    ./gradlew build
-
 fi
 
+ ./gradlew clean build
